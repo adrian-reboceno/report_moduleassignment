a89426a7eafd041ecdb3d7daa804b7e99d4c10c5
adrian-reboceno <adrian.rebocenno@outlook.com>
2025-01-29 01:01:49 -0600

    unificando con el helper report_ucnl

--- js/selects.js
+++ js/selects.js
@@ -1,52 +1,30 @@
-function get_subcategory(val) {
-    $.ajax({
-    type: "POST",
-    url: "get_subcategory.php",
-    data:'category_id='+val,
-    success: function(data){
-        $("#id_subcategory").html(data);
-    }
-    });
-}
-
-function get_semester(val) {
-    $.ajax({
-    type: "POST",
-    url: "get_semester.php",
-    data:'subcategory='+val,
-    success: function(data){
-        $("#id_semester").html(data);
-    }
-    });
-}
-
 function get_program(val) {
     $.ajax({
     type: "POST",
-    url: "get_program.php",
-    data:'subcategory='+val,
+    url: "get_data.php",
+    data:'flag=category&categoryid='+val,
     success: function(data){
         $("#id_program").html(data);
+    }
+    });
+}
+function get_semester(val) {
+    $.ajax({
+    type: "POST",
+    url: "get_data.php",
+    data:'flag=category&categoryid='+val,
+    success: function(data){
+        $("#id_semester").html(data);
     }
     });
 }
 function get_course(val) {
     $.ajax({
     type: "POST",
-    url: "get_course.php",
-    data:'category_id='+val,
+    url: "get_data.php",
+    data:'flag=course&categoryid='+val,
     success: function(data){
         $("#id_course").html(data);
-    }
-    });
-}
-function get_student(val) {
-    $.ajax({
-    type: "POST",
-    url: "get_student.php",
-    data:'course_id='+val,
-    success: function(data){
-        $("#id_student").html(data);
     }
     });
 }
